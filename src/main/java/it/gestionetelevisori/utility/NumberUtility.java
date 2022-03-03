864a82ef634a9ab77f47a36bd6c549e634ab950c
Emanuele Seminara <seminara.emanuele96@gmail.com>
2022-03-03 15:15:00 +0100

    Inserito NumberUtility

--- src/main/java/it/gestionetelevisori/utility/NumberUtility.java
+++ src/main/java/it/gestionetelevisori/utility/NumberUtility.java
@@ -1,5 +1,19 @@
 package it.gestionetelevisori.utility;
 
 public class NumberUtility {
+	public static Integer isInteger(String numeroInput) {
+		try {
+			return Integer.parseInt(numeroInput);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
+	public static Long isLong(String numeroInput) {
+		try {
+			return Long.parseLong(numeroInput);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
